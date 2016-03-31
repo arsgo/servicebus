a2b074deb4a286e843727e3b5bd44d81fa8d0a84
lib4go@163.com <lib4go@163.com>
2016-03-31 18:19:41 +0800

    ''

---
+++ spserver/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/colinyl/lib4go/logger"
+	"github.com/colinyl/servicebus/cluster"
+)
+
+var vlog, _ = logger.New("service provider", true)
+func main() {
+    
+       cluster.ServiceProvider.
+    
+    
+    
+    
+
+}
